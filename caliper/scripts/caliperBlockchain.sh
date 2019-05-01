b1d532956f21d2302bdde3daccf2a75f483133fb
Frinystrom <fridtjof.nystrom@gmail.com>
2019-05-01 16:28:24 +0200

    Caliper w/ benchmarks

---
+++ caliper/scripts/caliperBlockchain.sh
@@ -0,0 +1,41 @@
+#I1: ssh fabusr@158.39.75.116 "/home/fabusr/2org-hlf-deployment-prod/scripts/teardown.sh"
+#I2: ssh fabusr@158.37.63.164 "/home/fabusr/2org-hlf-deployment-prod/scripts/teardown.sh"
+#I3: ssh fabusr@158.37.63.165 "/home/fabusr/2org-hlf-deployment-prod/scripts/teardown.sh"
+#I4: ssh fabusr@158.37.63.175 "/home/fabusr/2org-hlf-deployment-prod/scripts/teardown.sh"
+#I5: ssh fabusr@158.37.63.184 "/home/fabusr/2org-hlf-deployment-prod/scripts/teardown.sh"
+#ssh-copy-id -i fabusr@158.37.63.164
+#ssh-copy-id fabusr@158.37.63.165
+#ssh-copy-id fabusr@158.37.63.175
+#ssh-copy-id fabusr@158.37.63.184
+#ssh-copy-id fabusr@158.39.75.116
+# 1. dep i1 2 3 4
+# i1: 158.39.86.116
+echo "Deploying instance 1."
+ssh fabusr@158.39.75.116 "cd /home/fabusr/2org-hlf-deployment-prod ; ./scripts/depIns1.sh"
+echo "Sleeping some time to let the orderers start up properly."
+sleep 2
+echo "Deploying other instances."
+ssh fabusr@158.37.63.164 "cd /home/fabusr/2org-hlf-deployment-prod ; ./scripts/depIns2.sh"
+ssh fabusr@158.37.63.165 "cd /home/fabusr/2org-hlf-deployment-prod ; ./scripts/depIns3.sh"
+ssh fabusr@158.37.63.175 "cd /home/fabusr/2org-hlf-deployment-prod ; ./scripts/depIns4.sh"
+ssh fabusr@158.37.63.184 "cd /home/fabusr/2org-hlf-deployment-prod ; ./scripts/depIns5.sh"
+#sleep 1
+echo "All done."
+#ssh fabusr@158.37.63.164 "cd /home/fabusr/2org-hlf-deployment-prod ; ./scripts/I2_createChannel.sh"
+#sleep 1
+
+#echo "Next step: run spreadBlockFromInstance2.sh from Macintosh."
+#./spreadBlockFromInstance2.sh
+#sleep 1
+
+
+#ssh fabusr@158.37.63.165 "cd /home/fabusr/2org-hlf-deployment-prod ; ./scripts/I3_joinChannel.sh"
+#ssh fabusr@158.37.63.175 "cd /home/fabusr/2org-hlf-deployment-prod ; ./scripts/I4_joinChannel.sh"
+#ssh fabusr@158.37.63.184 "cd /home/fabusr/2org-hlf-deployment-prod ; ./scripts/I5_joinChannel.sh"
+
+#sleep 1
+#echo "Next step: run I2_instantiate_CC.sh on Instance 2."
+#ssh fabusr@158.37.63.164 "cd /home/fabusr/2org-hlf-deployment-prod ; ./scripts/I2_instantiate_CC.sh"
+
+#sleep 5
+#ssh fabusr@158.37.63.165 "cd /home/fabusr/2org-hlf-deployment-prod ; ./scripts/generateData.sh"
